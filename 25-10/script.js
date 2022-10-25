9782b7a6e0e6c32188b9e046afae297a7079325d
Abdulla <talant.u.abdulla.pi.2-19@mail.ru>
2022-10-25 11:20:32 +0600

    feat: 25-10

---
+++ 25-10/script.js
@@ -0,0 +1,16 @@
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7 + c;
+let result = d;
+console.log(result);
+
+// let hour = prompt('введите час');
+// let minut = prompt('введите минуту');
+// let sec = prompt('введите секунды');
+// alert(`${hour}:${minut}:${sec}`)
+
+let num = 10;
+num = ++num;
+num = ++num;
+alert(num);
