c7c392bb4d735ec9eebd615cb880a669fb70b414
Abdulla <talant.u.abdulla.pi.2-19@mail.ru>
2022-10-26 11:09:18 +0600

    feat:26-10

---
+++ 26-10/script.js
@@ -0,0 +1,81 @@
+/*****  &&  *****/
+let age = 18;
+let name = 'Abdulla';
+let result = age > 17 && name == 'Abdulla';
+console.log(result);
+
+let age1 = 18;
+let name1 = 'Abdulla';
+let result1 = age1 > 18 && name1 == 'Abdulla';
+console.log(result1);
+
+let age2 = 18;
+let name2 = 'Abdulla';
+let result2 = age2 === '18' && name2 == 'Bektursun';
+console.log(result2);
+
+let age3 = 18;
+let name3 = 'Abdulla';
+let result3 = age3 >= 18 && name3 == 16;
+console.log(result3);
+
+
+
+
+/*****  ||  *****/
+
+let user_age = 20;
+let user_name = 'reyna'
+let res = user_age >= 20 || user_name === 'reyna';
+console.log(res);
+
+let user_age1 = 20;
+let user_name1 = 'reyna'
+let res1 = user_age1 >= 21 || user_name1 === 'rey';
+console.log(res1);
+
+let user_age2 = 20;
+let user_name2 = 'reyna'
+let res2 = user_age2 >= 20 || user_name2 === 'chember';
+console.log(res2);
+
+let user_age3 = 20;
+let user_name3 = 'reyna'
+let res3 = user_age3 >= 15 || user_name3 === 'reyna';
+console.log(res3);
+
+
+/*****  1  *****/
+let num = 10;
+num = num++;
+num = num+1;
+num = num--;
+console.log(num);
+
+/*****  2  *****/
+let num1 = 47;
+num1 = num1 += 7;
+num1 = num1 -=18;
+num1 = num1 *=10;
+num1 = num1 /= 15;
+alert(num1);
+
+/*****  3  *****/
+
+let num2 = 1;
+num2 = num2 += 12;
+num2 = num2 -=14;
+num1 = num2 *=5;
+num2 = num2 /= 7;
+num2 = num2 ++;
+num2 = num2 --;
+alert(num2);
+
+
+
+
+
+
+
+
+
